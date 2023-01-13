658fd650b8f9d1f36e936f8f4b7e2273e270f28c
SLeBron <sophieclebron@gmail.com>
2023-01-13 12:25:16 -0700

    imported react and icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,19 @@
-// import React from 'react';
+import React from 'react';
 import './footer.css';
+import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineMail } from 'react-icons/ai';
 
 function footer() {
     return (
+        <div className='footer'>
+            <a
+            >
 
+            </a>
+            <a
+            >
+                
+            </a>
+        </div>
     )
 }
 
