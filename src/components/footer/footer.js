d0427e8a092a7fecef68e5a6172a2a06332400ee
SLeBron <sophieclebron@gmail.com>
2023-01-13 12:47:04 -0700

    completed footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,16 +5,29 @@ import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineMail } from 'react-icons/a
 function footer() {
     return (
         <div className='footer'>
-            <a
+            <a 
+                href="https://github.com/sclebron"
+                target="_blank"
+                className="footerLink"
             >
-
+                <AiOutlineGithub className="footerIcon"/>
             </a>
             <a
+                href="https://www.linkedin.com/in/sophielebron/"
+                target="_blank"
+                className="footerLink"
             >
-                
+                <AiOutlineLinkedin className="footerIcon"/>
+            </a>
+            <a
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=sophieclebron@gmail.com"
+                target="_blank"
+                className="footerLink"
+            >
+                <AiOutlineMail className="footerIcon"/>
             </a>
         </div>
-    )
+    );
 }
 
 export default footer;
