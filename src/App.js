31200f709b8a25dd6eda81d2717969e1b11a46ce
SLeBron <sophieclebron@gmail.com>
2023-01-23 09:39:57 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import footer from './components/footer/footer.js';
+
 import './App.css';
 
 function App() {
