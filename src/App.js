57364e4ec292e243d938a9b201b11cc5fe577f5d
SLeBron <sophieclebron@gmail.com>
2023-01-23 10:35:11 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import footer from './components/footer/footer.js';
-
+import navBar from './components/navBar/navBar.js';
+import aboutMe from './pages/aboutMe/aboutMe.js';
+import contact from './pages/contact/contact.js';
+import homepage from './pages/homepage/homepage.js';
+import projects from './pages/projects/projects.js';
+import resume from './pages/resume/resume.js';
 import './App.css';
 
 function App() {
