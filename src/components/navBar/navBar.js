ee2faf25f3a5608e8af49fc1204a2a008eead758
SLeBron <sophieclebron@gmail.com>
2023-01-24 09:00:36 -0700

    redo navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -5,46 +5,7 @@ import Dropdown from "react-bootstrap/Dropdown";
 
 function Navigation() {
   return (
-    <Dropdown className="menu-toggle" align="end">
-      <Dropdown.Toggle
-        className="menutoggle"
-        variant="dark"
-        id="dropdown-basic"
-      >
-        <GiHamburgerMenu />
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu className="dropmenu" variant="dark">
-        <Dropdown.Item className="swing-in-top-fwd">
-          <Link
-            className="dropitem"
-            to={{ pathname: "/DG-Professional-Portfolio" }}
-          >
-            Home
-          </Link>
-        </Dropdown.Item>
-        <Dropdown.Item className="swing-in-top-fwd-1">
-          <Link className="dropitem" to={{ pathname: "/AboutMe" }}>
-            About Me
-          </Link>
-        </Dropdown.Item>
-        <Dropdown.Item className="swing-in-top-fwd-2">
-          <Link className="dropitem" to={{ pathname: "/Projects" }}>
-            Projects
-          </Link>
-        </Dropdown.Item>
-        <Dropdown.Item className="swing-in-top-fwd-3">
-          <Link className="dropitem" to={{ pathname: "/Contact" }}>
-            Contact
-          </Link>
-        </Dropdown.Item>
-        <Dropdown.Item className="swing-in-top-fwd-4">
-          <Link className="dropitem" to={{ pathname: "/Resume" }}>
-            Resume
-          </Link>
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    
   );
 }
 
