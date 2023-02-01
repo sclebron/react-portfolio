f790215428879b59a0f4e15ac1013b4fbba2889c
SLeBron <sophieclebron@gmail.com>
2023-02-01 13:32:47 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <homepage/>
           <Routes>
             <Route path=''/>
-            <Route path='./pages/aboutMe' element={<aboutMe/>}/>
+            <Route path='aboutMe' element={<aboutMe/>}/>
             <Route path=''/>
             <Route path=''/>
             <Route path=''/>
