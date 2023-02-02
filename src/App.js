3dfa2ebf9a88a3bf3dfa9a63c254c5e4f60f58ad
SLeBron <sophieclebron@gmail.com>
2023-02-02 11:19:43 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import footer from './components/footer/footer.js';
+import header from './components/header/header.js';
 import navBar from './components/navBar/navBar.js';
 import aboutMe from './pages/aboutMe/aboutMe.js';
 import contact from './pages/contact/contact.js';
@@ -13,7 +14,8 @@ function App() {
   return (
     <>
       <Router>
-        <homepage/>
+        <header />
+        <div>
           <Routes>
             <Route path=''/>
             <Route path='aboutMe' element={<aboutMe/>}/>
@@ -21,6 +23,8 @@ function App() {
             <Route path=''/>
             <Route path=''/>
           </Routes>
+          </div>
+        <footer />
       </Router>
     </>
   );
