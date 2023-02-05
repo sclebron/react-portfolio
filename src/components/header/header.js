1e533fd1d10d127c935ced0aac36cd8d072ca7a8
SLeBron <sophieclebron@gmail.com>
2023-02-05 12:34:45 -0700

    updated header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,3 +2,12 @@ import React from 'react';
 import navBar from '../navBar/navBar';
 import './navBar/navBar.css';
 
+function header() {
+    return (
+        <div>
+            <navBar/>
+        </div>
+    );
+}
+
+export default header;
