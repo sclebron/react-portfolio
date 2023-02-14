758456f8026f09c75f58c6125a9dc608c36eb887
SLeBron <sophieclebron@gmail.com>
2023-02-14 11:59:19 -0700

    updated navbar links

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -9,16 +9,13 @@ function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">About Me</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
