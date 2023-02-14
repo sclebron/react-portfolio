e0df8c9ca3f20d0ebcfb4290d9e411855da08938
SLeBron <sophieclebron@gmail.com>
2023-02-14 11:46:11 -0700

    changed navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -5,18 +5,29 @@ import Dropdown from "react-bootstrap/Dropdown";
 
 function NavBar() {
   return (
-    <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">About Me</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Contact</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Projects</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Resume</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
