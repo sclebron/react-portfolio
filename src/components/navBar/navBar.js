8b8c8a85e3714a3a54c0d5625746533410e2d7d1
SLeBron <sophieclebron@gmail.com>
2023-02-14 11:54:29 -0700

    added links to navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -15,6 +15,9 @@ function NavBar() {
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
