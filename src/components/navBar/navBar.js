3526b287eec8c6d1f1d0d8624101c070162638fe
SLeBron <sophieclebron@gmail.com>
2023-02-26 20:43:21 -0700

    fixed typo in navbar.js

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -11,7 +11,7 @@ function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/#AboutMe">About Me</Nav.Link>
+            <Nav.Link href="#AboutMe">About Me</Nav.Link>
             <Nav.Link href="#Contact">Contact</Nav.Link>
             <Nav.Link href="#Projects">Projects</Nav.Link>
             <Nav.Link href="#Resume">Resume</Nav.Link>
