04d6a6b40d3e1de918e84af68f17d3eac5846637
SLeBron <sophieclebron@gmail.com>
2023-02-26 20:56:02 -0700

    started contact page

---
+++ src/pages/contact/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contact.css';
+
+function Contact() {
+    return (
+        <div class="ex">
+            Sophie LeBron
+        </div>
+    )
+}
+
+export default Contact;
