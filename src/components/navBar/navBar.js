f17e3489a799df144b4a6b338d1a88f91189d21d
SLeBron <sophieclebron@gmail.com>
2023-03-09 13:58:52 -0700

    imported link to navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./navBar.css";
+import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
