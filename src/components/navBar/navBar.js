07abd994af5e40062321651cc8e56a2dd037394d
SLeBron <sophieclebron@gmail.com>
2023-03-09 14:04:17 -0700

    added navbar buttons

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -10,6 +10,21 @@ function NavBar() {
   return (
     <header className="headerNav">
       <div className="navBar">
+        <Link to="/AboutMe">
+          <button className="navBtn" id="aboutme">
+            AboutMe
+          </button>
+        </Link>
+        <Link to="/Contact">
+          <button className="navBtn" id="contact">
+            Contact
+          </button>
+        </Link>
+        <Link to="/Projects">
+          <button className="navBtn" id="projects">
+            Projects
+          </button>
+        </Link>
         <Link to="/Resume">
           <button className="navBtn" id="resume">
             Resume
