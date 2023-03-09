518c0c26e39cb3f99ba7f45da559016332467414
SLeBron <sophieclebron@gmail.com>
2023-03-09 14:20:28 -0700

    started projects page

---
+++ src/pages/projects/projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./projects.css";
+
+function Projects() {
+    return (
+        <div className="container">
+            <div className="header">Projects</div>
+                <div className="bio">
+                    <p></p>
+                </div>
+        </div>
+    );
+}
+
+export default Projects;
