93ed8784262196a50dce56d11302532b002b0fe1
SLeBron <sophieclebron@gmail.com>
2023-03-09 14:14:12 -0700

    deleted unnecessary code in navbar.js

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./navBar.css";
 import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-//# in url is causing issue. change the navigation and routes to not include that, may need new bootstrap navbar
 
 function NavBar() {
   return (
@@ -36,21 +32,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-// function NavBar() {
-//   return (
-//     <Navbar bg="light" expand="lg">
-//       <Container>
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="">Home</Nav.Link>
-//             <Nav.Link href="AboutMe">About Me</Nav.Link>
-//             <Nav.Link href="Contact">Contact</Nav.Link>
-//             <Nav.Link href="Projects">Projects</Nav.Link>
-//             <Nav.Link href="Resume">Resume</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
