7ad7c382fc432fa275c915612f4ae651b5d0b535
SLeBron <sophieclebron@gmail.com>
2023-03-16 21:25:34 -0600

    added comment

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -13,3 +13,5 @@ function Projects() {
 }
 
 export default Projects;
+
+//have big projects shown in big cards in center of screen, maybe 3-4 of these, underneath in smaller cards have smaller projects like bootcamp activities. these cards will be much smaller and there should be arrows on each side to scroll through. display a few then can click on arrow to display next chunk of a few activites, etc.
