188e059dc5603d70312f843cfe2d24e1b360bd35
SLeBron <sophieclebron@gmail.com>
2023-03-21 17:30:55 -0600

    started contact page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -3,7 +3,11 @@ import './contact.css';
 
 function Contact() {
     return (
-        <div class="title">Contact</div>
+        <div class="title">Contact
+            <div>Connect with me on Github!</div>
+            <div>Connect with me on LinkedIn!</div>
+            <div>Send me an email!</div>
+        </div>
     )
 }
 
