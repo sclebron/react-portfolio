994415323cb322ea7fcdf3b1fd9ca1c29fc67db9
SLeBron <sophieclebron@gmail.com>
2023-03-21 18:53:07 -0600

    changed homepage.js format

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -5,9 +5,9 @@ function Homepage() {
     return (
         <div className="name">
             Sophie LeBron
-        </div>,
-        <div className="title">
-            Full Stack Developer
+            <div className="title">
+                Full Stack Developer
+            </div>
         </div>
     );
 }
