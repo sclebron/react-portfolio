aa4e8fa04cc3113d35427126bd52d88559d7634b
SLeBron <sophieclebron@gmail.com>
2023-03-28 15:30:10 -0600

    adjusted homepage text

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -5,8 +5,10 @@ import './homepage.scss';
 function Homepage() {
     return (
         <div className="home">
+            <div className="text">
             <p>Hello, I'm Sophie LeBron</p>
             <p>I'm a full stack developer</p>
+            </div>
         </div>
     );
 }
