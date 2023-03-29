feb09b5e95c607480912bb17d5bb29f152dabbfa
SLeBron <sophieclebron@gmail.com>
2023-03-29 14:01:33 -0600

    updated firefly animation

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -3,9 +3,11 @@ import './homepage.css';
 
 function Homepage() {
     return (
+        <div className="firefly-container">
         <div className="home">
             <div className="name">Hello, I'm Sophie LeBron</div>
             <div className="title">I'm a full stack developer</div>
+        </div>
         </div>
     );
 }
