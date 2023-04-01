8ecf0f1e13d531b2003ad623d633fc7d2e23b7e4
SLeBron <sophieclebron@gmail.com>
2023-04-01 13:54:13 -0600

    updated particles

--- src/components/particles/particles.jsx
+++ src/components/particles/particles.jsx
@@ -3,8 +3,21 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
 function Particle() {
+
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
     return (
-        <div>Particle</div>
+        <Particles />
     )
 }
 
