2f67f8dcd4778ae6abdc6f10153ade825559d73b
SLeBron <sophieclebron@gmail.com>
2023-04-03 14:26:49 -0600

    changed span to h1

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -5,7 +5,7 @@ function Homepage() {
     return (
         <div className="firefly-container">
         <div className="home">
-            <h1 className="name">Hello, I'm <span className="highlight">Sophie LeBron</span>.</h1>
+            <h1 className="name">Hello, I'm <h1 className="highlight">Sophie LeBron</h1>.</h1>
             <h2 className="title">I'm a full stack developer.</h2>
         </div>
         </div>
