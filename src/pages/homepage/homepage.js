ccf4d67b3afe7473f1b2005a3c5d2247c9317355
SLeBron <sophieclebron@gmail.com>
2023-04-05 11:12:09 -0600

    deleted unnecessary container

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -3,11 +3,9 @@ import './homepage.css';
 
 function Homepage() {
     return (
-        <div className="firefly-container">
         <div className="home">
             <h1 className="name">Hello, I'm <mark className="name2">Sophie LeBron</mark>.</h1>
             <h2 className="title">I'm a full stack developer.</h2>
-        </div>
         </div>
     );
 }
