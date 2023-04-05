8159912e0431de26700888157cab1498f8cce3b3
SLeBron <sophieclebron@gmail.com>
2023-04-05 11:03:47 -0600

    changed homepage text styling

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -5,7 +5,7 @@ function Homepage() {
     return (
         <div className="firefly-container">
         <div className="home">
-            <h1 className="name">Hello, I'm <h1 className="name2">Sophie LeBron.</h1></h1>
+            <h1 className="name">Hello, I'm <mark className="name2">Sophie LeBron</mark>.</h1>
             <h2 className="title">I'm a full stack developer.</h2>
         </div>
         </div>
