8e56ff54f1b1008eb58415081440d200729cc066
SLeBron <sophieclebron@gmail.com>
2023-04-05 11:22:36 -0600

    updated homepage text styling

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -4,8 +4,12 @@ import './homepage.css';
 function Homepage() {
     return (
         <div className="home">
-            <h1 className="name">Hello, I'm <mark className="name2">Sophie LeBron</mark>.</h1>
-            <h2 className="title">I'm a full stack developer.</h2>
+            {/* <h1 className="name">Hello, I'm <mark className="name2">Sophie LeBron</mark>.</h1>
+            <h2 className="title">I'm a full stack developer.</h2> */}
+            <text className="text">
+                Hello, I'm Sophie LeBron.{"\n"} 
+                I'm a full stack developer.
+            </text>
         </div>
     );
 }
