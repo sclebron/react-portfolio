7c5ac714c85caf9e39b6518d95e2434c6a344d46
SLeBron <sophieclebron@gmail.com>
2023-04-06 11:47:43 -0600

    updated homepage text

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -4,8 +4,6 @@ import './homepage.css';
 function Homepage() {
     return (
         <div className="home">
-            {/* <h1 className="name">Hello, I'm <mark className="name2">Sophie LeBron</mark>.</h1>
-            <h2 className="title">I'm a full stack developer.</h2> */}
             <text className="text">
                 Hello, I'm <mark className="name">Sophie LeBron.</mark> <br />
                 I'm a full stack developer.
