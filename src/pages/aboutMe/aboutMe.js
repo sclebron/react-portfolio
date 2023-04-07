5ac0015f9244a23eb51ccc1b00933a06eb3d9f7c
SLeBron <sophieclebron@gmail.com>
2023-04-07 11:41:11 -0600

    updated bio

--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -6,8 +6,8 @@ function AboutMe() {
         <div className="container">
             <div className="title">About Me</div>
                 <div className="bio">
-                    Hi! My name is Sophie and I'm a full-stack developer based in Denver, CO.
-                    I currently work as a teaching assistant for the University of Utah Coding Boot Camp. I was a student in the University of Denver Coding Boot Camp from July-October 2022. During my time in the Boot Camp I learned HTML5, CSS3, Javascript, jQuery, Express.js, React.js, Node.js, MySQL, and MongoDB. I also learned how to collaborate with others to complete coding projects, as the Boot Camp involved three group projects. Prior to the Boot Camp I had no coding experience. In college I studied physical therapy, and up to two days before beginning the Boot Camp I was working as a rehab tech at an inpatient rehabilitation hospital. In my coding work I enjoy using my problem-solving skills, creativity, adaptability, and curiosity.
+                    Hi! My name is Sophie and I'm a full-stack developer based in Denver, CO. <br />
+                    I currently work as a teaching assistant for the University of Utah Coding Boot Camp. I was a student in the University of Denver Coding Boot Camp from July-October 2022. During my time in the Boot Camp I learned HTML5, CSS3, Javascript, jQuery, Express.js, React.js, Node.js, MySQL, and MongoDB. I also learned how to collaborate with others to complete coding projects, as the Boot Camp involved three group projects. Prior to the Boot Camp I had no coding experience. In college I studied physical therapy, and up to two days before beginning the Boot Camp I was working as a rehab tech at an inpatient rehabilitation hospital. In my coding work I enjoy using my problem-solving skills, creativity, adaptability, and curiosity. <br />
                     When I'm not coding, I like to spend my time skiing, hiking, traveling, cooking, playing piano, being active, learning new things, and being with my boyfriend and our dog, Peanut!
                 </div>
         </div>
