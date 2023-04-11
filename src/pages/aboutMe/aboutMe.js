7cdc018e05535d2134ff63e13f2cd1c793b3d794
SLeBron <sophieclebron@gmail.com>
2023-04-10 18:09:06 -0600

    updated line break in about me bio

--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -6,9 +6,9 @@ function AboutMe() {
         <div className="container">
             <div className="title">About Me</div>
                 <p className="bio">
-                    <span className="brlarge">Hi! My name is Sophie and I'm a full-stack developer based in Denver, CO.</span>
-                    <span className="brlarge">I currently work as a teaching assistant for the University of Utah Coding Boot Camp. I was a student in the University of Denver Coding Boot Camp from July-October 2022. During my time in the Boot Camp I learned HTML5, CSS3, Javascript, jQuery, Express.js, React.js, Node.js, MySQL, and MongoDB. I also learned how to collaborate with others to complete coding projects, as the Boot Camp involved three group projects. Prior to the Boot Camp I had no coding experience. In college I studied physical therapy, and up to two days before beginning the Boot Camp I was working as a rehab tech at an inpatient rehabilitation hospital. In my coding work I enjoy using my problem-solving skills, creativity, adaptability, and curiosity.</span>
-                    <span className="brlarge">When I'm not coding, I like to spend my time skiing, hiking, traveling, cooking, playing piano, being active, learning new things, and being with my boyfriend and our dog, Peanut!</span>
+                    <span>Hi! My name is Sophie and I'm a full-stack developer based in Denver, CO.</span> <br />
+                    <span>I currently work as a teaching assistant for the University of Utah Coding Boot Camp. I was a student in the University of Denver Coding Boot Camp from July-October 2022. During my time in the Boot Camp I learned HTML5, CSS3, Javascript, jQuery, Express.js, React.js, Node.js, MySQL, and MongoDB. I also learned how to collaborate with others to complete coding projects, as the Boot Camp involved three group projects. Prior to the Boot Camp I had no coding experience. In college I studied physical therapy, and up to two days before beginning the Boot Camp I was working as a rehab tech at an inpatient rehabilitation hospital. In my coding work I enjoy using my problem-solving skills, creativity, adaptability, and curiosity.</span> <br />
+                    <span>When I'm not coding, I like to spend my time skiing, hiking, traveling, cooking, playing piano, being active, learning new things, and being with my boyfriend and our dog, Peanut!</span>
                 </p>
         </div>
     );
