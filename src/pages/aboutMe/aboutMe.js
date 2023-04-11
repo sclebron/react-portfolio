f6326331bc620106a769acbb1a61e2620635e387
SLeBron <sophieclebron@gmail.com>
2023-04-11 11:24:47 -0600

    added new particles files

--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./aboutMe.css";
-import '../components/particles/particles.jsx';
+import './particles.jsx';
 
 function AboutMe() {
     return (
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "./projects.css";
-import '../components/particles/particles.jsx';
+import './particles.jsx';
 
 function Project() {
     const projects = [
