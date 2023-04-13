82c77a1f41c848764fb56a5ad95912cf31fccdb1
SLeBron <sophieclebron@gmail.com>
2023-04-13 14:38:04 -0600

    updated contact form

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,17 +1,54 @@
-import React from 'react';
+// import React from 'react';
+// import './contact.css';
+// import Particles from './particles.jsx';
+
+// function Contact() {
+//     return (
+//         <div className="contact">
+//             <Particles />
+//             <div class="title">Contact</div>
+//                 <div className="formContainer">
+                    
+//                 </div>
+//         </div>
+//     )
+// }
+
+// export default Contact;
+
+import React, { useRef } from 'react';
 import './contact.css';
 import Particles from './particles.jsx';
+import emailjs from '@emailjs/browser';
 
-function Contact() {
-    return (
-        <div className="contact">
-            <Particles />
-            <div class="title">Contact</div>
-                <div className="formContainer">
-                    
-                </div>
-        </div>
-    )
+const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_03cqjd4', 'template_w1momlc', form.current, 'exWicqHUee0tIYOXK')
+        .then((result) => {
+            e.target.reset();
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
+return <div className="contactContainer">
+        <Particles />
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
+    </div>
 }
 
 export default Contact;
--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,30 +1,33 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+// import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
 
-const Contact = () => {
+// const Contact = () => {
 
-    const form = useRef();
+//     const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+//     const sendEmail = (e) => {
+//         e.preventDefault();
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
+//         emailjs.sendForm('service_03cqjd4', 'template_w1momlc', form.current, 'exWicqHUee0tIYOXK')
+//         .then((result) => {
+//             e.target.reset();
+//             console.log(result.text);
+//         }, (error) => {
+//             console.log(error.text);
+//         });
+//     };
 
-return <div>
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
-        </form>
-    </div>
-}
+// return <div>
+//         <form ref={form} onSubmit={sendEmail}>
+//             <label>Name</label>
+//             <input type="text" name="user_name" />
+//             <label>Email</label>
+//             <input type="email" name="user_email" />
+//             <label>Message</label>
+//             <textarea name="message" />
+//             <input type="submit" value="Send" />
+//         </form>
+//     </div>
+// }
+
+// export default Contact;
