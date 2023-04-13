1ec9c14d33076134402a1d30c3ff76218e45b086
SLeBron <sophieclebron@gmail.com>
2023-04-13 14:05:52 -0600

    updated contact form

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,4 +1,18 @@
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
 return <div>
         <form ref={form} onSubmit={sendEmail}>
             <label>Name</label>
