f473202e2b5c62e5155593fe169e70539087a50d
SLeBron <sophieclebron@gmail.com>
2023-04-13 14:04:31 -0600

    started contact form

---
+++ src/pages/contact/contact.jsx
@@ -0,0 +1,13 @@
+const Contact = () => {
+return <div>
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
+    </div>
+}
