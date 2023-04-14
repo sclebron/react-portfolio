d94a77a0a7b263330ad03de647125e331a2bcb57
SLeBron <sophieclebron@gmail.com>
2023-04-14 00:18:48 -0600

    updated contact form

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -40,15 +40,17 @@ const Contact = () => {
 return <div className="contactContainer">
         <Particles />
         <div class="title">Contact</div>
-        <form ref={form} onSubmit={sendEmail} className="contactForm">
-            <label className="label">Name</label>
-            <input type="text" name="user_name" className="input" />
-            <label className="label">Email</label>
-            <input type="email" name="user_email" className="input" />
-            <label className="label">Message</label>
-            <textarea name="message" className="textarea" />
-            <input type="submit" value="Submit" className="input" />
-        </form>
+        <div className="contactForm">
+            <form ref={form} onSubmit={sendEmail}>
+                <label className="label">Name</label>
+                <input type="text" name="user_name" className="input" />
+                <label className="label">Email</label>
+                <input type="email" name="user_email" className="input" />
+                <label className="label">Message</label>
+                <textarea name="message" className="textarea" />
+                <input type="submit" value="Submit" className="input" />
+            </form>
+        </div>
     </div>
 }
 
