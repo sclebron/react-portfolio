20719eacde4d67d3dda66d79050b9496bf4cee87
SLeBron <sophieclebron@gmail.com>
2023-05-04 14:17:17 -0600

    changed bio

--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
                 <div className="bio">
                     Hi! My name is Sophie and I'm a full stack developer based in Denver, CO. <br />
                     <br />
-                    I currently work as a teaching assistant for the University of Utah Coding Boot Camp, which teaches HTML5, CSS3, Javascript, jQuery, Express.js, React.js, Node.js, MySQL, and MongoDB. In my coding work I enjoy using my problem-solving skills, creativity, adaptability, and curiosity. Currently, I am working on some MERN personal projects, including an app to document visits to National Parks! <br />
+                    I currently work as a teaching assistant for the University of Utah Coding Boot Camp, which teaches HTML5, CSS3, Javascript, jQuery, Express.js, React.js, Node.js, MySQL, and MongoDB. (add sentence about group projects) I am working on some MERN personal projects, including an app to . In my coding work I enjoy using my problem-solving skills, creativity, adaptability, and curiosity. <br />
                     <br />
                     When I'm not coding, I like to spend my time skiing, hiking, traveling, cooking, playing piano, being active, learning new things, and being with my boyfriend and our dog, Peanut!
                 </div>
