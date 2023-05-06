f777a4b95bfba22ae956b5dd09bbe2528f20ff31
SLeBron <sophieclebron@gmail.com>
2023-05-06 12:58:57 -0600

    added projects mapping to project cards

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -58,14 +58,11 @@ function Project() {
                 {projects.map((projects) => (
                     <Col xs="4">
                         <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Img src={projects.img} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Title>{projects.title}</Card.Title>
+                                <Card.Text>{projects.description}</Card.Text>
+                                <Button variant="primary" href={projects.link} target="_blank">Visit site</Button>
                             </Card.Body>
                         </Card>
                     </Col>
