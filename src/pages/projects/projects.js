37b319de6333cfbce0f71f34db6d52da3a33e4bd
SLeBron <sophieclebron@gmail.com>
2023-05-13 15:27:35 -0600

    deleted button from projects page

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./projects.css";
+import '../components/footer.css';
 import Particles from './particles.jsx';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
